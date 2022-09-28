fc92ba927b910f7c302c2d07ba5cbdd084657266
Pablo Navarro Zepeda <88807082+panaze@users.noreply.github.com>
2022-09-28 08:50:08 -0500

    Clase 23 de Septiembre

---
+++ Actividad en clase 23 Sep/Actividad en clase 23 Sep/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Actividad en clase 23 Sep
+//
+//  Created by Alumno on 23/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
